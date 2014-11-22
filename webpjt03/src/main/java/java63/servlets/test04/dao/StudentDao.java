4ae0f05a6d8d68516b51668abe66a2c695ebfc3d
borashin <borashin@192.168.0.40>
2014-11-22 18:46:36 +0900

    jo?

--- webpjt03/src/main/java/java63/servlets/test04/dao/StudentDao.java
+++ webpjt03/src/main/java/java63/servlets/test04/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package java63.servlets.test04.dao;
 
+import java.util.HashMap;
+import java.util.List;
 import java63.servlets.test04.domain.Student;
 
 import org.apache.ibatis.session.SqlSession;
@@ -26,8 +28,7 @@ public class StudentDao {
 	    
 	    try {
 	      return sqlSession.selectOne(
-	        "java02.test19.server.StudentDao.selectOne", 
-	        no);
+	        "java02.test19.server.StudentDao.selectOne", no); 
 	    } finally {
 	      sqlSession.close();
 	    }
@@ -45,5 +46,42 @@ public class StudentDao {
 	 
 	}
 	
+	public void delete(int no) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			sqlSession.delete(
+			"java02.test19.server.StudentDao.delete", no);
+			sqlSession.commit();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public List<Student> studentselectList(int pageNo, int pageSize) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		HashMap<String,Object> paramMap = new HashMap<>();
+		paramMap.put("startIndex", ((pageNo - 1) * pageSize));
+		paramMap.put("pageSize", pageSize);
+		
+		try {
+			return sqlSession.selectList(
+			"java02.test19.server.StudentDao.selectList",
+			paramMap);
+		} finally {
+			sqlSession.close();
+		}
+		
+	}
+	
+	public void insert(Student student) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			sqlSession.insert(
+					"java02.test19.server.Student.insert", student);
+		} finally {
+			sqlSession.close();
+		}
+	}
 
 }
